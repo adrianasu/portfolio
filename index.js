23f5c7336028166a85e58df1f2d7dcf61db56d2a
Adriana Suarez <agsmq@yahoo.com>
2019-02-28 10:03:01 -0600

    Fix mobile nav bar to close on click anywhere on the body.

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
+function toggleOpenClass(event){
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    $('.site-nav').toggleClass('open');
+}
 
-function watchHamburguer() {
-    $('.js-nav-container').on('click', '.js-nav-toggle-button', function (event) {
-        event.stopPropagation();
-        event.stopImmediatePropagation();
-        $('.site-nav').toggleClass('open');
+function watchMobileNavBar() {
+    // Open/close menu when the hamburguer is clicked
+    $('.js-nav-container').on('click', '.js-nav-toggle-button', toggleOpenClass);
+
+    // If an option is selected, close the menu
+    $('.js-nav-container').on('click', 'a', function(event){
+        if( $('.site-nav').hasClass('open') === true ){
+            toggleOpenClass(event);
+        }
     })
 
-    $('.js-nav-container').on('click', 'a', function (event) {
-        if( $('.site-nav').hasClass('open') === true ){
-            event.stopPropagation();
-            event.stopImmediatePropagation();
-            $('.site-nav').toggleClass('open');
+    // If the menu is open and user clicks on any part
+    // of the body, close the menu.
+    $('body').on('click', function(event){
+        if ($('.site-nav').hasClass('open') === true) {
+            toggleOpenClass(event);
         }
     })
 }
 
-$(watchHamburguer);
+$(watchMobileNavBar);
